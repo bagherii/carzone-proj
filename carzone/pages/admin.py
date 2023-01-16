ca6ccc47fe21209b7347d2cdc7137fc95e7cd61b
hajar <hajarbagheri825@gmail.com>
2023-01-16 11:10:37 +0330

    completed team model

--- carzone/pages/admin.py
+++ carzone/pages/admin.py
@@ -1,3 +1,18 @@
+from django.utils.html import format_html
 from django.contrib import admin
-
+from.models import Team
 # Register your models here.
+
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumnail(self, object):
+        return format_html('<img  src="{}" width="40" style="border-radius:50px "/>'.format(object.photo.url))
+    thumnail.short_description = "photo"
+    list_display = ("id", "first_name",
+                    "last_name", "created_date")
+    list_display_links = ("id", "first_name")
+    search_fields = ("first_name", "last_name", "designation")
+    list_filter = ("designation",)
+
+
+admin.site.register(Team, TeamAdmin)
--- carzone/pages/models.py
+++ carzone/pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    facebook_link = models.URLField(max_length=200)
+    google_plus_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100, default="")
+    created_date = models.DateTimeField(auto_now_add=True)
+    photo: models.ImageField(upload_to='photos/%Y/%m/%d')
+
+    def __str__(self):
+        return self.first_name
